bd67263448ab8882e50085129a0bd450409c9f15
spvagelis <spirou.vagelis@gmail.com>
2020-10-15 10:09:31 +0300

    Complete find all channels method.

---
+++ Slacky/Model/Channel.swift
@@ -0,0 +1,16 @@
+//
+//  Channel.swift
+//  Slacky
+//
+//  Created by vagelis spirou on 14/10/20.
+//  Copyright © 2020 vagelis spirou. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var title: String!
+    public private(set) var description: String!
+    public private(set) var id: String!
+   
+}
---
+++ Slacky/Services/MessageService.swift
@@ -0,0 +1,54 @@
+//
+//  MessageService.swift
+//  Slacky
+//
+//  Created by vagelis spirou on 14/10/20.
+//  Copyright © 2020 vagelis spirou. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+
+class MessageService {
+    
+    static let instance = MessageService()
+    
+    var channels = [Channel]()
+    
+    func findAllChannel(completion: @escaping CompletionHandler) {
+        
+        Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+            
+            if response.result.error == nil {
+                
+                guard let data = response.data else { return }
+                
+                
+                do {
+                    if let json = try JSON(data: data).array {
+                        for item in json {
+                            let name = item["name"].stringValue
+                            let channelDescription = item["description"].stringValue
+                            let id = item["_id"].stringValue
+                            
+                            let channel = Channel(title: name, description: channelDescription, id: id)
+                            self.channels.append(channel)
+                        }
+                        completion(true)
+                    }
+                    
+                } catch {
+                    print("I can't get the data!")
+                    completion(false)
+                }
+                
+                
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+}
