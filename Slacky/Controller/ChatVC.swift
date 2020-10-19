85aee14a28f5b11ed0b0c0c974ca9019ed63947e
spvagelis <spirou.vagelis@gmail.com>
2020-10-19 16:21:29 +0300

    Fetching Messages.

--- Slacky/Controller/ChatVC.swift
+++ Slacky/Controller/ChatVC.swift
@@ -63,15 +63,32 @@ class ChatVC: UIViewController {
         
         let channelName = MessageService.instance.selectedChannel?.title ?? ""
         channelNameLbl.text = "#\(channelName)"
+        getMessages()
     }
     
     func onLoginGetMessages() {
         MessageService.instance.findAllChannel { (success) in
             
             if success {
-                
                 // Do stuff with channels
+                if MessageService.instance.channels.count > 0 {
+                    
+                    MessageService.instance.selectedChannel = MessageService.instance.channels[0]
+                    self.updateWithChannel()
+                } else {
+                    self.channelNameLbl.text = "No channels yet!"
+                }
             }
+        }
+    }
+    
+    func getMessages() {
+        
+        guard let channelId = MessageService.instance.selectedChannel?.id else { return }
+        MessageService.instance.findAllMessagesFromSpecificChannel(channelId: channelId) { (success) in
+            
+            
+            
         }
     }
 }
---
+++ Slacky/Model/Message.swift
@@ -0,0 +1,20 @@
+//
+//  Message.swift
+//  Slacky
+//
+//  Created by vagelis spirou on 19/10/20.
+//  Copyright © 2020 vagelis spirou. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    
+    public private(set) var message: String!
+    public private(set) var userName: String!
+    public private(set) var channelId: String!
+    public private(set) var userAvatar: String!
+    public private(set) var userAvatarColor: String!
+    public private(set) var id: String!
+    public private(set) var timeStamp: String!
+}
